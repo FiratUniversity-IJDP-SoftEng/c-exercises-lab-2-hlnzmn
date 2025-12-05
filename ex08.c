44e1893f87ce42100d031098dfd015efa13503ce
hlnzmn <148447720+hlnzmn@users.noreply.github.com>
2025-12-05 18:16:16 +0300

    Correct array handling and average calculation
    Fix array usage and calculation of sum and average.

--- ex08.c
+++ ex08.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+int main() {
+  int array[10] ;
+int i ;
+int sum = 0 ;
+float average ;
+  for(i=0;i<10;i++);
+  printf("%d ", arr[i]);
+  sum += arr[i];
+average = sum / 10.0;
 
+printf("\nSum = %d\n", sum);
+printf("Average = %.2f\n", average);
+
+return 0 ;
+  }
