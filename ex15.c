04e56708a29c06013f8fb60fba3c906aaff2b188
hlnzmn <148447720+hlnzmn@users.noreply.github.com>
2025-12-05 18:57:09 +0300

    Update ex15.c

--- ex15.c
+++ ex15.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
 
+int main() {
+    int n = 5;
+
+    for (int i = 1; i <= n; i++) {
+        for (int s = 0; s < n - i; s++)
+            printf(" ");
+
+        for (int j = 1; j <= i; j++)
+            printf("%d", j);
+
+        for (int j = i - 1; j >= 1; j--)
+            printf("%d", j);
+
+        printf("\n");
+    }
+
+   
+    for (int i = n - 1; i >= 1; i--) {
+        for (int s = 0; s < n - i; s++)
+            printf(" ");
+
+        for (int j = 1; j <= i; j++)
+            printf("%d", j);
+
+        for (int j = i - 1; j >= 1; j--)
+            printf("%d", j);
+
+        printf("\n");
+    }
+
+    return 0;
+}
