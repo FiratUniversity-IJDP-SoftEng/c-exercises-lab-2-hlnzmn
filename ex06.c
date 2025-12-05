44d68796f2f9e2198b218bf104cc0d95cc8c2688
hlnzmn <148447720+hlnzmn@users.noreply.github.com>
2025-12-05 16:59:04 +0300

    Implement logic to find numbers divisible by 3
    Added logic to store and print numbers divisible by 3 from 100 to 50.

--- ex06.c
+++ ex06.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+int main() {
+  int arr[51];     
+    int i;  
+    for (i = 100; i >= 50; i--) {
+        if (i % 3 == 0) {    
+            arr[index] = i;  
+            index++;          
+        }
+    }
+printf("Numbers divisible by 3 from 100 to 50:\n");
+    for (i = 0; i < index; i++) {
+        printf("%d ", arr[i]);
+    }
 
+    return 0;
+}
