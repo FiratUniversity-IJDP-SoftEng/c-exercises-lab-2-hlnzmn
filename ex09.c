4d895565a5c20fe0930184dc313edc64aa62b39e
hlnzmn <148447720+hlnzmn@users.noreply.github.com>
2025-12-05 18:54:20 +0300

    Implement sum, count, and average calculator
    This program calculates the sum, count, and average of user-inputted numbers until -1 is entered.

--- ex09.c
+++ ex09.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
 
+int main() {
+    int num;
+    int sum = 0, count = 0;
+
+    while (1) {
+        printf("Sayi gir (-1 bitirir): ");
+        scanf("%d", &num);
+
+        if (num == -1)
+            break;
+
+        sum += num;
+        count++;
+    }
+
+    if (count > 0) {
+        printf("Toplam: %d\n", sum);
+        printf("Adet: %d\n", count);
+        printf("Ortalama: %.2f\n", (float)sum / count);
+    } else {
+        printf("Hic sayi girilmedi.\n");
+    }
+
+    return 0;
+}
