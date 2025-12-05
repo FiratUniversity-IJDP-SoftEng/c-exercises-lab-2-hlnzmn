0829920fbe15d021c9aa42ee61a96613ebe2deb3
hlnzmn <148447720+hlnzmn@users.noreply.github.com>
2025-12-05 18:55:38 +0300

    Add factorial calculation in ex12.c
    Implement factorial calculation for user input.

--- ex12.c
+++ ex12.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int main() {
+    int n;
+
+    do {
+        printf("Faktoriyel (0-50): ");
+        scanf("%d", &n);
+    } while (n < 0 || n > 50);
+
+    for (int i = 1; i <= n; i++) {
+       
+    }
+
+    printf("%d! = %lld\n", n, fact);
+
+    return 0;
+}
